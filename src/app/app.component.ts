559f25ec0809b698ed724b1b0b74727cdccee930
mchammer91 <yankeesbatboy91@gmail.com>
2025-04-23 05:08:34 -0500

    basic grid with issues using VMware Clarity Design System

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IssueListComponent } from './issue-list/issue-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [IssueListComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
